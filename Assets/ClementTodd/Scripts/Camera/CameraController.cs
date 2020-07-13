3f75cc7a34e5ec7b55e81fd5a3fe541204869d03
clementtodd <clement.todd@snowedin.ca>
2020-07-13 14:36:04 -0400

    Smoother camera movement

--- Assets/ClementTodd/Scripts/Camera/CameraController.cs
+++ Assets/ClementTodd/Scripts/Camera/CameraController.cs
@@ -22,10 +22,11 @@ namespace ClementTodd_v0_0_1
         }
 
         public float lookSpeed = 1f;
+        public float maxLookSpeedDelta = 1f;
 
         [Range(0f, 1f)] public float mouseSensitivity = 0.1f;
-        private const float verticalMouseSensitivity = 0.1f;
 
+        private Vector2 rawLook = Vector2.zero;
         private Vector2 look = Vector2.zero;
         private bool isMouseLook = false;
         public static UnityEvent lookUpdatedEvent = new UnityEvent();
@@ -38,13 +39,35 @@ namespace ClementTodd_v0_0_1
 
         public void OnLook(InputAction.CallbackContext context)
         {
-            look = context.action.ReadValue<Vector2>();
+            rawLook = context.action.ReadValue<Vector2>();
             isMouseLook = context.control.device.name == "Mouse";
-            lookUpdatedEvent?.Invoke();
         }
 
         private void Update()
         {
+            // Smoothly interpolate look input towards its raw input value to make camera movement feel less stiff
+            // (Use snappier smooting if looking with a mouse)
+            if (!Mathf.Approximately(Vector2.Distance(look, rawLook), 0f))
+            {
+                if (isMouseLook)
+                {
+                    if (rawLook.sqrMagnitude > look.sqrMagnitude)
+                    {
+                        look = Vector2.Lerp(look, rawLook, maxLookSpeedDelta * Time.deltaTime);
+                    }
+                    else
+                    {
+                        look = rawLook;
+                    }
+                }
+                else
+                {
+                    look = Vector2.MoveTowards(look, rawLook, maxLookSpeedDelta * Time.deltaTime);
+                }
+                lookUpdatedEvent?.Invoke();
+            }
+
+            // Apply smoothed look input to the current camera if applicable
             ICinemachineCamera virtualCamera = brain.ActiveVirtualCamera;
 
             if (virtualCamera is CinemachineFreeLook)
@@ -54,12 +77,10 @@ namespace ClementTodd_v0_0_1
                 Vector2 freeLook = new Vector2(
                     freeLookCamera.m_XAxis.m_InvertInput ? -look.x : look.x,
                     freeLookCamera.m_YAxis.m_InvertInput ? -look.y : look.y);
-                freeLook.y *= Mathf.Abs(look.y);
 
                 if (isMouseLook)
                 {
                     freeLook *= mouseSensitivity;
-                    freeLook.y *= verticalMouseSensitivity;
                 }
 
                 freeLookCamera.m_XAxis.Value += freeLook.x * 180f * lookSpeed * Time.deltaTime;
