9973a71cfba3a5d55a23970fb8368ad98ec76c27
clementtodd <clement.todd@snowedin.ca>
2020-11-07 12:53:37 -0500

    Added new input to camera controller

--- Assets/ClementTodd_v0_0_1/Scripts/Camera/CameraController.cs
+++ Assets/ClementTodd_v0_0_1/Scripts/Camera/CameraController.cs
@@ -6,7 +6,7 @@ using UnityEngine.Events;
 namespace ClementTodd_v0_0_1
 {
     [RequireComponent(typeof(CinemachineBrain))]
-    public class CameraController : MonoBehaviour
+    public class CameraController : MonoBehaviour, IInputReceiver
     {
         private CinemachineBrain _brain;
         private CinemachineBrain brain
@@ -21,15 +21,39 @@ namespace ClementTodd_v0_0_1
             }
         }
 
+        [SerializeField]
+        private bool enableLookInput = true;
+        public bool EnableLookInput
+        {
+            get
+            {
+                return enableLookInput;
+            }
+
+            set
+            {
+                if (value)
+                {
+                    InputManager.onInputReceived += OnInputReceived;
+                }
+                else
+                {
+                    InputManager.onInputReceived -= OnInputReceived;
+                }
+                enableLookInput = value;
+            }
+        }
+
         public float lookSpeed = 1f;
         public float maxLookSpeedDelta = 1f;
 
         private Vector2 rawLook = Vector2.zero;
         private Vector2 look = Vector2.zero;
 
-        public void OnLook(InputAction.CallbackContext context)
+        private void Awake()
         {
-            rawLook = context.action.ReadValue<Vector2>();
+            // Set the look enabled property to whatever the value is in the inspector to trigger event subscription.
+            EnableLookInput = enableLookInput;
         }
 
         private void Update()
@@ -53,6 +77,14 @@ namespace ClementTodd_v0_0_1
 
                 freeLookCamera.m_XAxis.Value += freeLook.x * 180f * lookSpeed * Time.deltaTime;
                 freeLookCamera.m_YAxis.Value += freeLook.y * lookSpeed * Time.deltaTime;
+            }
+        }
+
+        public void OnInputReceived(GamepadInput input, InputAction.CallbackContext context)
+        {
+            if (input == GamepadInput.RightStick)
+            {
+                rawLook = context.action.ReadValue<Vector2>();
             }
         }
     }
