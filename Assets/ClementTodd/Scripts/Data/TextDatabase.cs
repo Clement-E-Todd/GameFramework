257417b8ecc1e5cc35b5c018ae2c5fa926e0017b
clementtodd <clement.todd@snowedin.ca>
2020-11-07 12:20:46 -0500

    Gave rough dialogue to NPC2 and NPC3 in PrototypeGym1

---
+++ Assets/ClementTodd/Scripts/Data/TextDatabase.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace ClementTodd_v0_0_1
+{
+    [CreateAssetMenu(fileName = "TextDatabase", menuName = "Clement Todd/Data/Text Database", order = 0)]
+    public class TextDatabase : ScriptableObject
+    {
+        [System.Serializable]
+        public class Entry
+        {
+
+
+            [System.Serializable]
+            public class Language
+            {
+                public string text;
+            }
+
+            public Language english;
+            public Language test;
+        }
+    }
+}
