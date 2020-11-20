5b65a8cf05c7b920ef616984185405d09c37b0fc
clementtodd <clement.todd@snowedin.ca>
2020-11-20 18:16:57 -0500

    Pause and change speed of dialogue text animation partway through with animation markup tags

--- Assets/ClementTodd/Scripts/Dialogue/DialogueCanvas.cs
+++ Assets/ClementTodd/Scripts/Dialogue/DialogueCanvas.cs
@@ -1,5 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
 using TMPro;
+using UnityEngine;
 
 namespace ClementTodd
 {
@@ -27,8 +28,86 @@ namespace ClementTodd
         public float defaultFadeInDelay = 0.01f;
         public float defaultFadeInDuration = 0.01f;
         private float characterAnimationStartTime = float.MinValue;
-        private CharacterAnimationData[] characterAnimationData;
+        private List<CharacterAnimationData> characterAnimationData;
         const float fadeOutDuration = 0.1f;
+
+        #region CharacterAnimationCommand Definition
+        private struct CharacterAnimationCommand
+        {
+            public string command;
+            public string[] args;
+            public string fullText;
+
+            public CharacterAnimationCommand(string sourceText, int startIndex)
+            {
+                command = string.Empty;
+                args = null;
+                fullText = string.Empty;
+
+                bool readingCommand = false;
+                bool readingArgs = false;
+
+                List<string> argList = new List<string>();
+                int argIndex = 0;
+
+                for (int sourceIndex = startIndex; sourceIndex < sourceText.Length; sourceIndex++)
+                {
+                    char character = sourceText[sourceIndex];
+                    fullText += character;
+
+                    // Start listening for the command name when '{' is found.
+                    if (character == '{')
+                    {
+                        if (readingCommand || command.Length > 0)
+                            Debug.LogError("Tried to start a new command but a command is already being processed,");
+                        else if (!readingCommand)
+                            readingCommand = true;
+                        continue;
+                    }
+
+                    // End processing when '}' is found.
+                    if (character == '}')
+                    {
+                        break;
+                    }
+
+                    // While reading a command name, record each letter until ':' is found.
+                    if (readingCommand)
+                    {
+                        if (character != ':')
+                        {
+                            command += character;
+                        }
+                        else
+                        {
+                            readingCommand = false;
+                            readingArgs = true;
+                        }
+
+                        continue;
+                    }
+
+                    // While reading arguments, record each letter, seperating arguments with ','.
+                    if (readingArgs)
+                    {
+                        while (argIndex >= argList.Count)
+                        {
+                            argList.Add(string.Empty);
+                        }
+
+                        if (character != ',')
+                            argList[argIndex] += character;
+                        else
+                            argIndex++;
+
+                        continue;
+                    }
+                }
+
+                args = argList.ToArray();
+            }
+        }
+        #endregion
 
         private void Awake()
         {
@@ -62,11 +141,13 @@ namespace ClementTodd
 
         public void SetText(string text)
         {
-            dialogueBoxLabel.text = text;
+            InitCharacterAnimationDataFromText(text);
+
+            dialogueBoxLabel.text = RemoveAnimationTags(text);
             dialogueBoxLabel.ForceMeshUpdate();
 
             ShowDialogueBox();
-            DoShowTextAnimation();
+            StartShowTextAnimation();
         }
 
         public bool TryAdvanceText()
@@ -86,11 +167,26 @@ namespace ClementTodd
 
             if (richTextIndex < dialogueBoxLabel.text.Length)
             {
+                // Remove the currently-displayed text from the UI
                 dialogueBoxLabel.text = dialogueBoxLabel.text.Remove(0, richTextIndex);
                 dialogueBoxLabel.ForceMeshUpdate();
 
+                // Clear the removed text's animation data, reducing the delay time before the remaining letters should be shown
+                characterAnimationData.RemoveRange(0, textIndex);
+                if (characterAnimationData.Count > 0)
+                {
+                    float delay = characterAnimationData[0].fadeInDelay;
+                    for (int i = 0; i < characterAnimationData.Count; i++)
+                    {
+                        CharacterAnimationData data = characterAnimationData[i];
+                        data.fadeInDelay -= delay;
+                        characterAnimationData[i] = data;
+                    }
+                }
+
+                // Show the next part of the tex
                 ShowDialogueBox();
-                DoShowTextAnimation();
+                StartShowTextAnimation();
 
                 return true;
             }
@@ -100,26 +196,135 @@ namespace ClementTodd
             }
         }
 
-        private void DoShowTextAnimation()
+        private void InitCharacterAnimationDataFromText(string sourceText)
         {
-            textVisible = true;
-            characterAnimationStartTime = Time.unscaledTime;
-            characterAnimationData = new CharacterAnimationData[dialogueBoxLabel.textInfo.characterCount];
+            characterAnimationData = new List<CharacterAnimationData>();
 
+            // Define animation properties shared between multiple characters
             float totalFadeInDelay = 0f;
+            float delayMultiplier = 1f;
+            float fadeInDuration = defaultFadeInDuration;
+            bool readingRichTextMarkup = false;
 
-            for (int i = 0; i < dialogueBoxLabel.textInfo.characterCount; i++)
+            for (int sourceIndex = 0; sourceIndex < sourceText.Length; sourceIndex++)
             {
-                if (i > 0)
+                // Define one-time animation properties for this character
+                float pauseDuration = 0f;
+
+                // Process character animation markup
+                while (sourceText[sourceIndex] == '{')
                 {
-                    totalFadeInDelay += defaultFadeInDelay;
+                    // Read the command from the source text
+                    CharacterAnimationCommand command = new CharacterAnimationCommand(sourceText, sourceIndex);
+
+                    // Skip the source index ahead to the end of the command
+                    if (command.fullText.Length > 0)
+                        sourceIndex += command.fullText.Length;
+                    else
+                        break;
+
+                    // Process the command:
+                    switch (command.command.ToLower())
+                    {
+                        // Add a one-time pause between characters with the the indicated duration.
+                        case "pause":
+                            {
+                                bool success = (command.args.Length > 0 && float.TryParse(command.args[0], out pauseDuration));
+                                if (!success)
+                                    Debug.Log("Invalid argument(s) for command 'Pause'.");
+                            }
+                            break;
+
+                        // Multiply the delay duration between characters until the end of the line (or until this command is called again).
+                        case "multiplydelay":
+                            {
+                                bool success = (command.args.Length > 0 && float.TryParse(command.args[0], out delayMultiplier));
+                                if (!success)
+                                    Debug.Log("Invalid argument for command 'MultiplyDelay'.");
+                            }
+                            break;
+
+                        // Multiply the fade-in duration for each character until the end of the line (or until this command is called again).
+                        case "fadeintime":
+                            {
+                                bool success = (command.args.Length > 0 && float.TryParse(command.args[0], out fadeInDuration));
+                                if (!success)
+                                    Debug.Log("Invalid argument for command 'FadeInTime'.");
+                            }
+                            break;
+
+                        default:
+                            {
+                                Debug.LogWarningFormat("Unknown command '{0}'.", command.command);
+                            }
+                            break;
+                    }
                 }
-                characterAnimationData[i].fadeInDelay = totalFadeInDelay;
-                characterAnimationData[i].fadeInDuration = defaultFadeInDuration;
+
+                // Skip this character if it is rich text markup
+                if (sourceText[sourceIndex] == '<')
+                {
+                    readingRichTextMarkup = true;
+                }
+                else if (sourceText[sourceIndex] == '>')
+                {
+                    readingRichTextMarkup = false;
+                    continue;
+                }
+                if (readingRichTextMarkup)
+                {
+                    continue;
+                }
+
+                // Calculate the delay duration for this character (time between the full text's
+                // animation beginning and this character appearing).
+                totalFadeInDelay += pauseDuration > 0f ? pauseDuration : defaultFadeInDelay * delayMultiplier;
+
+                // Create the animation data for this character
+                CharacterAnimationData characterData = new CharacterAnimationData();
+
+                characterData.fadeInDelay = totalFadeInDelay;
+                characterData.fadeInDuration = fadeInDuration;
+
+                characterAnimationData.Add(characterData);
             }
         }
 
-        private void DoHideTextAnimation()
+        private string RemoveAnimationTags(string sourceText)
+        {
+            string text = string.Empty;
+            bool skip = false;
+
+            for (int sourceIndex = 0; sourceIndex < sourceText.Length; sourceIndex++)
+            {
+                if (sourceText[sourceIndex] == '{')
+                {
+                    skip = true;
+                }
+                else if (sourceText[sourceIndex] == '}')
+                {
+                    skip = false;
+                    continue;
+                }
+
+                if (skip)
+                {
+                    continue;
+                }
+
+                text += sourceText[sourceIndex];
+            }
+
+            return text;
+        }
+
+        private void StartShowTextAnimation()
+        {
+            textVisible = true;
+            characterAnimationStartTime = Time.unscaledTime;
+        }
+
+        private void StartHideTextAnimation()
         {
             textVisible = false;
             characterAnimationStartTime = Time.unscaledTime;
